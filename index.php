f48adc25eae3ef6e169c56872c9a9a615328928f
Nina09900 <sabrinatatata11@gmail.com>
2026-05-06 10:38:25 -0300

    Commtid Inicial

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+echo "Exemplo  \n";
+$nota1 = readline("Digite a nota 1: ");
+$nota2 = readline("Digite a nota 2: ");
+$nota3 = readline("Digite a nota 3: ");
+$nota4 = readline("Bigite a nota 4: ");
+$media = ($nota1 + $nota2 + $nota3 + $nota4)/ 4;
+
+echo "Sua media é Media = $media";
+
+?>
