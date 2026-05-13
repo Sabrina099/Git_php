00142cd4b7ea0ccc790e4d4960351844ab109eba
Sabrina099 <00001108699686sp@al.educacao.sp.gov.br>
2026-05-13 11:37:33 -0300

    Alteração na Nova Branch!

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
 echo "Exemplo  \n";
-$nota1 = readline("Digite a nota 1: ");
-$nota2 = readline("Digite a nota 2: ");
-$nota3 = readline("Digite a nota 3: ");
-$nota4 = readline("Bigite a nota 4: ");
+$nota1 = readline("Digite a nota da primeira prova: ");
+$nota2 = readline("Digite a nota da segunda prova: ");
+$nota3 = readline("Digite a nota da terceira prova: ");
+$nota4 = readline("Bigite a nota da quarta prova: ");
 $media = ($nota1 + $nota2 + $nota3 + $nota4)/ 4;
 $freq = readline("Digite Frequência: ");
 
@@ -24,7 +24,7 @@ elseif($media <= 6 && $freq >= 75){
     }
 }
 elseif($media >= 6 && $freq <= 75){
-    echo "Aluno Reprovado por Falta! Lamento nada pode ser feito!";
+    echo "Aluno Reprovado por Falta!Lamento nada pode ser feito";
 }
 else{
 }
