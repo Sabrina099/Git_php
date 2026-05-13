f0d1948cfda7a7be07640daa6b373503eab56fc6
Sabrina099 <00001108699686sp@al.educacao.sp.gov.br>
2026-05-13 10:54:36 -0300

    Alteração

--- index.php
+++ index.php
@@ -5,25 +5,25 @@ $nota2 = readline("Digite a nota 2: ");
 $nota3 = readline("Digite a nota 3: ");
 $nota4 = readline("Bigite a nota 4: ");
 $media = ($nota1 + $nota2 + $nota3 + $nota4)/ 4;
-$freq = readline("Digite sua Frequencia: ");
+$freq = readline("Digite Frequencia: ");
 
 echo "Sua media é Media = $media";
 
-if($media >= 6 && $frequencia >= 75){
+if($media >= 6 && $freq >= 75){
     echo "Aluno Aprovado!";
 }
-elseif($media <= 6 && $frequencia >= 75){
+elseif($media <= 6 && $freq >= 75){
     echo "Aluno Reprovado por Nota!";
     
     $notaR = readline("Digite a nota de recuperação");
     $mediaf = $media + $notaR;
     if($mediaf > 5){
-        echo "Você foi Aprovado";
+        echo "Voce foi Aprovado";
     }else{
         echo "Reprovado, nós vemos ano que vem";
     }
 }
-elseif($media >= 6 && $frequencia <= 75){
+elseif($media >= 6 && $freq <= 75){
     echo "Aluno Reprovado por Falta, nada pode ser feito!";
 }
 else{
